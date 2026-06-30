d926464bb65fcb8d2ad1fa11b4078d1ae4b9a8d3
nikhil_ubuntu <nikhilpore03@gmail.com>
2026-06-30 19:18:45 +0530

    fix

--- server/app/src/main/java/ussoi/Http/ServerHttpInitializer.java
+++ server/app/src/main/java/ussoi/Http/ServerHttpInitializer.java
@@ -6,6 +6,9 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import ussoi.SessionHandler.ApiHandler;
 import ussoi.WebSocket.Route.WebSocketRouter;
 import ussoi.Security.AuthenticationService.WebSocketAuthHandler;
@@ -18,10 +21,22 @@ public class ServerHttpInitializer extends ChannelInitializer<Channel> {
             .toAbsolutePath()
             .normalize();
 
+
+    private final SslContext sslCtx;
+
+    public ServerHttpInitializer() throws Exception {
+        SelfSignedCertificate ssc = new SelfSignedCertificate();
+        sslCtx = SslContextBuilder
+                .forServer(ssc.certificate(), ssc.privateKey())
+                .build();
+    }
+
     @Override
     protected void initChannel(Channel ch) {
 
         ChannelPipeline p = ch.pipeline();
+
+        p.addFirst(sslCtx.newHandler(ch.alloc()));
 
         p.addLast(new HttpServerCodec());
         p.addLast(new HttpObjectAggregator(10 * 1024 * 1024));
@@ -29,19 +44,17 @@ public class ServerHttpInitializer extends ChannelInitializer<Channel> {
         p.addLast(new ApiHandler());
 
         p.addLast(new WebSocketServerProtocolHandler(
-                        "/ws",          // base websocket path
-                        null,           // subprotocols
-                        true,           // checkStartsWith → allow /ws/*
-                        8 * 1024 * 1024, // max frame payload length 8 MB changing this mat throttle stream
-                        true,           // allow extensions
-                        true,           // allow mask mismatch
-                        true           // don't forward pong frames DownStream
-                )
-        );
+                "/ws",
+                null,
+                true,
+                8 * 1024 * 1024,
+                true,
+                true,
+                true
+        ));
+
         p.addLast(new WebSocketAuthHandler());
         p.addLast("webSocketRouter", new WebSocketRouter());
-
-
 
         p.addLast(new StaticFileHandler(root));
     }
